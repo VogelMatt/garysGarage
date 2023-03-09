79f46038efa6f3bc50a4da998d703060ca7bd23f
ToastyandTasty <mahvogel@gmail.com>
2023-03-09 11:08:07 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace garysGarage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
